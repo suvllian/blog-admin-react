66f985f7ec8e4a4104e6c0f9e3a253ef97b54bc4
宋青松 <suvlliansong@163.com>
2017-06-27 00:02:08 +0800

    初始化，使用antd开发。

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+import router from './routes.js'
+import 'antd/dist/antd.css'
+
+
+render(router, document.getElementById('app'))
---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Router, HashRouter, Match, Route,
+ Link, hashHistory, IndexRoute, Redirect, IndexLink } from 'react-router-dom'
+
+import Login from './page/login/index.jsx'
+
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+	      { this.props.children }
+			</div>
+			);
+	}
+}
+
+const routes = (
+  <HashRouter history={hashHistory} >
+    <App>
+      <Route path="/" component={Login} />
+    </App>
+  </HashRouter>
+)
+
+export default routes;
